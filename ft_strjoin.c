eddf31c5a22f5f499641c333b33b8adcbc762e80
Charlie D Badger <156419225+CharlieDBadger@users.noreply.github.com>
2025-02-04 18:07:44 +0100

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -30,26 +30,30 @@
  * - A newly allocated string containing the concatenation of 's1' and 's2',
  *   or NULL if memory allocation fails.
  */
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*s3;
-	char	*s3rtn;
-	int		i;
-	int		j;
+	char	*ptr;
+	size_t	len1;
+	size_t	len2;
 
-	s3 = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (s3 == NULL)
+	if (!s1)
+		s1 = ft_strdup("");
+	if (!s1 || !s2)
 		return (NULL);
-	s3rtn = s3;
-	i = 0;
-	j = 0;
-	while (s1[i] != '\0')
-		s3[j++] = s1[i++];
-	i = 0;
-	while (s2[i] != '\0')
-		s3[j++] = s2[i++];
-	s3[j] = '\0';
-	return (s3rtn);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	s3 = malloc(len1 + len2 + 1);
+	if (!s3)
+		return (free(s1), NULL);
+	ptr = s3;
+	while (*s1)
+		*ptr++ = *s1++;
+	while (*s2)
+		*ptr++ = *s2++;
+	*ptr = '\0';
+	free(s1 - len1);
+	return (s3);
 }
 /*
 #include <stdio.h>
